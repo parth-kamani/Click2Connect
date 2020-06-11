c9de931d4442e88d0e0feb63d9bcbaca79da11ed
parth-kamani <parth.k.kamani@gmail.com>
2020-06-11 12:36:34 -0400

    Commit C2C

---
+++ CaptureScreenshot/CaptureScreenshot/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace CaptureScreenshot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.btnRecord = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.t1 = new System.Windows.Forms.Timer(this.components);
+            this.lblh = new System.Windows.Forms.Label();
+            this.lbls = new System.Windows.Forms.Label();
+            this.lblm = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCapture.Location = new System.Drawing.Point(23, 44);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(82, 36);
+            this.btnCapture.TabIndex = 0;
+            this.btnCapture.Text = "Capture";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // btnRecord
+            // 
+            this.btnRecord.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRecord.Location = new System.Drawing.Point(111, 44);
+            this.btnRecord.Name = "btnRecord";
+            this.btnRecord.Size = new System.Drawing.Size(82, 36);
+            this.btnRecord.TabIndex = 2;
+            this.btnRecord.Text = "Record";
+            this.btnRecord.UseVisualStyleBackColor = true;
+            this.btnRecord.Click += new System.EventHandler(this.btnRecord_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnd.Location = new System.Drawing.Point(199, 44);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(82, 36);
+            this.btnEnd.TabIndex = 3;
+            this.btnEnd.Text = "End";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // t1
+            // 
+            this.t1.Interval = 1000;
+            this.t1.Tick += new System.EventHandler(this.t1_Tick);
+            // 
+            // lblh
+            // 
+            this.lblh.AutoSize = true;
+            this.lblh.Font = new System.Drawing.Font("Bookman Old Style", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblh.Location = new System.Drawing.Point(12, 9);
+            this.lblh.Name = "lblh";
+            this.lblh.Size = new System.Drawing.Size(51, 32);
+            this.lblh.TabIndex = 5;
+            this.lblh.Text = "00";
+            // 
+            // lbls
+            // 
+            this.lbls.AutoSize = true;
+            this.lbls.Font = new System.Drawing.Font("Bookman Old Style", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbls.Location = new System.Drawing.Point(222, 9);
+            this.lbls.Name = "lbls";
+            this.lbls.Size = new System.Drawing.Size(51, 32);
+            this.lbls.TabIndex = 6;
+            this.lbls.Text = "00";
+            // 
+            // lblm
+            // 
+            this.lblm.AutoSize = true;
+            this.lblm.Font = new System.Drawing.Font("Bookman Old Style", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblm.Location = new System.Drawing.Point(111, 9);
+            this.lblm.Name = "lblm";
+            this.lblm.Size = new System.Drawing.Size(51, 32);
+            this.lblm.TabIndex = 7;
+            this.lblm.Text = "00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bookman Old Style", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(57, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 32);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "h :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bookman Old Style", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(156, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 32);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "m :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bookman Old Style", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(267, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 32);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "s";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 96);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblm);
+            this.Controls.Add(this.lbls);
+            this.Controls.Add(this.lblh);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnRecord);
+            this.Controls.Add(this.btnCapture);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Screen Recoder";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.Button btnRecord;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.Timer t1;
+        private System.Windows.Forms.Label lblh;
+        private System.Windows.Forms.Label lbls;
+        private System.Windows.Forms.Label lblm;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CaptureScreenshot/CaptureScreenshot/Form1.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using AForge.Video.FFMPEG;
+using System.Runtime.InteropServices;
+using System.Drawing.Drawing2D;
+using System.Net;
+using System.IO;
+using Click2Connect;
+
+namespace CaptureScreenshot
+{
+    public partial class Form1 : Form
+    {
+        int temp;
+        int h, m, s;
+        Bitmap bmp, cursorBMP;
+        Graphics gr;
+        Rectangle screen = new Rectangle();
+        VideoFileWriter write = new VideoFileWriter();
+        int cursorX = 0;
+        int cursorY = 0;
+        int height = Screen.PrimaryScreen.Bounds.Height;
+        int width = Screen.PrimaryScreen.Bounds.Width; 
+        static string path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Click2Connect");
+        string path2 = path+ "\\Captured Images";
+        string path1 = path + "\\Recorded Video";   
+
+        public Form1()
+        {
+            InitializeComponent();
+            temp = 0;
+            h = m = s = 0;
+            btnEnd.Enabled = false;
+            SetFormPosition();
+            if (!Directory.Exists(path1))
+            {
+                System.IO.Directory.CreateDirectory(path1);
+            }
+            if (!Directory.Exists(path2))
+            {
+                System.IO.Directory.CreateDirectory(path2);
+            }
+            lblh.ForeColor = Color.SlateGray;
+            lblm.ForeColor = Color.SlateGray;
+            lbls.ForeColor = Color.SlateGray;
+            label1.ForeColor = Color.SlateGray;
+            label2.ForeColor = Color.SlateGray;
+            label3.ForeColor = Color.SlateGray;
+        }
+
+        private void SetFormPosition()
+        {
+            this.StartPosition = FormStartPosition.Manual;
+            this.Left = Screen.PrimaryScreen.WorkingArea.Right - this.Width;
+            this.Top = Screen.PrimaryScreen.WorkingArea.Bottom - this.Height;
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                this.WindowState = FormWindowState.Minimized;
+                System.Threading.Thread.Sleep(500);
+                string d = null;
+                d = DateTime.Now.ToString(".yyyy.MM.dd,HH.mm.ss");
+                string name = "CAPIMG" + d;
+                Bitmap bm = new Bitmap(width, height);
+                Graphics g = Graphics.FromImage(bm);
+                g.CopyFromScreen(0, 0, 0, 0, bm.Size);
+                bm.Save(path2 +"\\"+ name + ".png", ImageFormat.Png);
+
+                this.WindowState = FormWindowState.Normal;
+                MessageBox.Show("Image Captured" );
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Capturing");
+                MessageBox.Show(ex.Message);
+            }
+        }
+             
+       
+        private void btnRecord_Click(object sender, EventArgs e)
+        {
+           if (btnRecord.Text == "Record")
+           {
+
+                lblh.ForeColor = Color.Black;
+                lblm.ForeColor = Color.Black;
+                lbls.ForeColor = Color.Black;
+                label1.ForeColor = Color.Black;
+                label2.ForeColor = Color.Black;
+                label3.ForeColor = Color.Black;
+                if (temp == 0)
+                {
+                    string d = null;
+                    d = DateTime.Now.ToString(".yyyy.MM.dd,HH.mm.ss");
+                    string name = "CAPVID" + d;
+                    write.Open(path1 + "\\" + name + ".wmv", width, height, 25, VideoCodec.WMV1, 50000000);
+                }
+                screen.Height = height;
+                screen.Width = width;
+                screen.Location = new Point(0, 0);
+                t1.Enabled = true;
+                t1.Start();
+                btnRecord.Text = "Pause";
+                btnEnd.Enabled = true;
+            }
+            else
+            {
+                temp = 1;
+                t1.Stop();
+                btnRecord.Text = "Record";
+                btnEnd.Enabled = false;
+                lblh.ForeColor = Color.SlateGray;
+                lblm.ForeColor = Color.SlateGray;
+                lbls.ForeColor = Color.SlateGray;
+                label1.ForeColor = Color.SlateGray;
+                label2.ForeColor = Color.SlateGray;
+                label3.ForeColor = Color.SlateGray;
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            t1.Stop();
+            write.Close();
+            Click2Connect.Selector ent = new Click2Connect.Selector();
+            //Click2Connect.Click2Connect cl = new Click2Connect.Click2Connect(ent);
+            this.Hide();
+            ent.ShowDialog();
+            this.Close();
+            this.Dispose();
+        }
+
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            t1.Stop();
+            write.Close();
+            btnRecord.Text = "Record";
+            MessageBox.Show("Saved");
+            temp = 0;
+            btnEnd.Enabled = false;
+            s = m = h = 0;
+            lbls.Text = az(s);
+            lblm.Text = az(m);
+            lblh.Text = az(h);
+            t1.Enabled = false;
+            lblh.ForeColor = Color.SlateGray;
+            lblm.ForeColor = Color.SlateGray;
+            lbls.ForeColor = Color.SlateGray;
+            label1.ForeColor = Color.SlateGray;
+            label2.ForeColor = Color.SlateGray;
+            label3.ForeColor = Color.SlateGray;
+        }
+
+        private void t1_Tick(object sender, EventArgs e)
+        {
+            try
+            {   
+                s++;
+                time(s);
+                bmp = new Bitmap(width,height);
+                cursorBMP = CaptureCursor(ref cursorX, ref cursorY);
+                screen = new Rectangle(cursorX, cursorY, cursorBMP.Width, cursorBMP.Height);
+                gr = Graphics.FromImage(bmp);
+                gr.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+                gr.DrawImage(cursorBMP, screen);
+                gr.Flush();
+                for (int i = 0; i <= 4; i++)
+                {
+                    write.WriteVideoFrame(bmp);
+                }
+                bmp = new Bitmap(width, height);
+                cursorBMP = CaptureCursor(ref cursorX, ref cursorY);
+                screen = new Rectangle(cursorX, cursorY, cursorBMP.Width, cursorBMP.Height);
+                gr = Graphics.FromImage(bmp);
+                gr.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+                gr.DrawImage(cursorBMP, screen);
+                gr.Flush();
+                for (int i = 0; i <= 4; i++)
+                {
+                    write.WriteVideoFrame(bmp);
+                }
+                bmp = new Bitmap(width, height);
+                cursorBMP = CaptureCursor(ref cursorX, ref cursorY);
+                screen = new Rectangle(cursorX, cursorY, cursorBMP.Width, cursorBMP.Height);
+                gr = Graphics.FromImage(bmp);
+                gr.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+                gr.DrawImage(cursorBMP, screen);
+                gr.Flush();
+                for (int i = 0; i <= 4; i++)
+                {
+                    write.WriteVideoFrame(bmp);
+                }
+                bmp = new Bitmap(width, height);
+                cursorBMP = CaptureCursor(ref cursorX, ref cursorY);
+                screen = new Rectangle(cursorX, cursorY, cursorBMP.Width, cursorBMP.Height);
+                gr = Graphics.FromImage(bmp);
+                gr.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+                gr.DrawImage(cursorBMP, screen);
+                gr.Flush();
+                for (int i = 0; i <= 4; i++)
+                {
+                    write.WriteVideoFrame(bmp);
+                }
+                bmp = new Bitmap(width, height);
+                cursorBMP = CaptureCursor(ref cursorX, ref cursorY);
+                screen = new Rectangle(cursorX, cursorY, cursorBMP.Width, cursorBMP.Height);
+                gr = Graphics.FromImage(bmp);
+                gr.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+                gr.DrawImage(cursorBMP, screen);
+                gr.Flush();
+                for (int i = 0; i <= 4; i++)
+                {
+                    write.WriteVideoFrame(bmp);
+                }              
+            }
+            catch (Exception ex)
+            {
+                t1.Stop();
+                t1.Start();
+            }
+        }
+
+        private string az(double str)
+        {
+            if(str<=9)
+            {
+                return "0" + str;
+            }
+            else
+            {
+                return str.ToString();
+            }
+        }
+        void time(int c)
+        {
+            
+            if(s>59)
+            {
+                m++;
+                s = 0;
+            }
+            if(m>59)
+            {
+                h++;
+                m = 0;
+            }
+
+            lbls.Text = az(s);
+            lblm.Text = az(m);
+            lblh.Text = az(h);
+        }
+
+        static Bitmap CaptureCursor(ref int x, ref int y)
+        {
+            Bitmap bmp;
+            IntPtr hicon;
+            Win32Stuff.CURSORINFO ci = new Win32Stuff.CURSORINFO();
+            Win32Stuff.ICONINFO icInfo;
+            ci.cbSize = Marshal.SizeOf(ci);
+            if (Win32Stuff.GetCursorInfo(out ci))
+            {
+                if (ci.flags == Win32Stuff.CURSOR_SHOWING)
+                {
+                    hicon = Win32Stuff.CopyIcon(ci.hCursor);
+                    if (Win32Stuff.GetIconInfo(hicon, out icInfo))
+                    {
+                        x = ci.ptScreenPos.x - ((int)icInfo.xHotspot);
+                        y = ci.ptScreenPos.y - ((int)icInfo.yHotspot);
+                        Icon ic = Icon.FromHandle(hicon);
+                        bmp = ic.ToBitmap();
+
+                        return bmp;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+
+    class Win32Stuff
+    {
+
+        #region Class Variables
+
+        public const int SM_CXSCREEN = 0;
+        public const int SM_CYSCREEN = 1;
+
+        public const Int32 CURSOR_SHOWING = 0x00000001;
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ICONINFO
+        {
+            public bool fIcon;         // Specifies whether this structure defines an icon or a cursor. A value of TRUE specifies 
+            public Int32 xHotspot;     // Specifies the x-coordinate of a cursor's hot spot. If this structure defines an icon, the hot 
+            public Int32 yHotspot;     // Specifies the y-coordinate of the cursor's hot spot. If this structure defines an icon, the hot 
+            public IntPtr hbmMask;     // (HBITMAP) Specifies the icon bitmask bitmap. If this structure defines a black and white icon, 
+            public IntPtr hbmColor;    // (HBITMAP) Handle to the icon color bitmap. This member can be optional if this 
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public Int32 x;
+            public Int32 y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct CURSORINFO
+        {
+            public Int32 cbSize;        // Specifies the size, in bytes, of the structure. 
+            public Int32 flags;         // Specifies the cursor state. This parameter can be one of the following values:
+            public IntPtr hCursor;          // Handle to the cursor. 
+            public POINT ptScreenPos;       // A POINT structure that receives the screen coordinates of the cursor. 
+        }
+
+        #endregion
+
+
+        #region Class Functions
+
+        [DllImport("user32.dll", EntryPoint = "GetDesktopWindow")]
+        public static extern IntPtr GetDesktopWindow();
+
+        [DllImport("user32.dll", EntryPoint = "GetDC")]
+        public static extern IntPtr GetDC(IntPtr ptr);
+
+        [DllImport("user32.dll", EntryPoint = "GetSystemMetrics")]
+        public static extern int GetSystemMetrics(int abc);
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowDC")]
+        public static extern IntPtr GetWindowDC(Int32 ptr);
+
+        [DllImport("user32.dll", EntryPoint = "ReleaseDC")]
+        public static extern IntPtr ReleaseDC(IntPtr hWnd, IntPtr hDc);
+
+
+        [DllImport("user32.dll", EntryPoint = "GetCursorInfo")]
+        public static extern bool GetCursorInfo(out CURSORINFO pci);
+
+        [DllImport("user32.dll", EntryPoint = "CopyIcon")]
+        public static extern IntPtr CopyIcon(IntPtr hIcon);
+
+        [DllImport("user32.dll", EntryPoint = "GetIconInfo")]
+        public static extern bool GetIconInfo(IntPtr hIcon, out ICONINFO piconinfo);
+
+
+        #endregion
+    }
+}
